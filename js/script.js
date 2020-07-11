2b4a27b31d692bb48525419ac95fb13340364f61
Rahul2K20 <63788480+Rahul2K20@users.noreply.github.com>
2020-07-12 01:37:29 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+$(window).on("load", function() {
+
+	$(".loader .inner").fadeOut(500, function() {
+		$(".loader").fadeOut(750);
+	});
+
+	$(".items").isotope({
+		filter: '*',
+		animationOptions: {
+			duration: 1500,
+			easing: 'linear',
+			queue: false
+		}
+	});
+
+})
+
+
 $(document).ready(function() {
 
 	$('#slides').superslides({
@@ -7,7 +25,7 @@ $(document).ready(function() {
 	});
 
 	var typed = new Typed(".typed", {
-		strings: ["Programmer.", "Web Developer.", "Student."],
+		strings: ["Sales Intern.", "Web Developer.", "Student."],
 		typeSpeed: 70,
 		loop: true,
 		startDelay: 1000,
@@ -27,47 +45,110 @@ $(document).ready(function() {
 	        768:{
 	            items:3
 	        },
-					938:{
-							items:4
-					}
+	        938:{
+	            items:4
+	        }
 	    }
-});
+	});
 
 
 
-var skillsTopOffset = $(".skillsSection").offset().top;
-var statsTopOffset = $(".statsSection").offset().top;
-var countUpFinished = false;
 
-$(window).scroll(function(){
-	if(window.pageYOffset > skillsTopOffset - $(window).height()+ 200){
-		$('.chart').easyPieChart({
-		  easing: 'easeInOut',
-			barColor: '#fff',
-			trackColor: false,
-			scaleColor: false,
-			lineWidth: 4,
-			size: 152,
-			onStep: function(from,to,percent) {
-				$(this.el).find('.percent').text(Math.round(percent));
+
+	var skillsTopOffset = $(".skillsSection").offset().top;
+	var statsTopOffset = $(".statsSection").offset().top;
+	var countUpFinished = false;
+	$(window).scroll(function() {
+
+		if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+
+			$('.chart').easyPieChart({
+		        easing: 'easeInOut',
+		        barColor: '#fff',
+		        trackColor: false,
+		        scaleColor: false,
+		        lineWidth: 4,
+		        size: 152,
+		        onStep: function(from, to, percent) {
+		        	$(this.el).find('.percent').text(Math.round(percent));
+		        }
+		    });
+
+
+		}
+
+
+		if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+			$(".counter").each(function() {
+				var element = $(this);
+				var endVal = parseInt(element.text());
+
+				element.countup(endVal);
+			})
+
+			countUpFinished = true;
+
+		}
+
+
+	});
+
+
+	$("[data-fancybox]").fancybox();
+
+	$("#filters a").click(function() {
+
+		$("#filters .current").removeClass("current");
+		$(this).addClass("current");
+
+		var selector = $(this).attr("data-filter");
+
+		$(".items").isotope({
+			filter: selector,
+			animationOptions: {
+				duration: 1500,
+				easing: 'linear',
+				queue: false
 			}
-
 		});
 
+		return false;
+	});
+
+
+
+	$("#navigation li a").click(function(e) {
+		e.preventDefault();
+
+		var targetElement = $(this).attr("href");
+		var targetPosition = $(targetElement).offset().top;
+		$("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
+
+	});
+
+
+
+	const nav = $("#navigation");
+	const navTop = nav.offset().top;
+
+	$(window).on("scroll", stickyNavigation);
+
+	function stickyNavigation() {
+
+		var body = $("body");
+
+		if($(window).scrollTop() >= navTop) {
+			body.css("padding-top", nav.outerHeight() + "px");
+			body.addClass("fixedNav");
+		}
+		else {
+			body.css("padding-top", 0);
+			body.removeClass("fixedNav");
+		}
+
+
+
+
 	}
-
-	if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height()+ 200){
-				$(".counter").each(function(){
-					var element = $(this);
-					var endVal = parseInt(element.text());
-					element.countup(endVal);
-				})
-				countUpFinished = true; 
-	}
-
-
-});
-
-
 
 });
