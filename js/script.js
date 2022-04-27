1fea9599c6ec2d038e31227b3d54327e235d3671
Rahul Nagarajan <63788480+Rahul2K20@users.noreply.github.com>
2022-04-27 14:52:24 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ $(document).ready(function() {
 	});
 
 	var typed = new Typed(".typed", {
-		strings: ["Software Developer.", "ML Enthusiast", "Student."],
+		strings: ["Software Developer.", "Gamer.", "Student."],
 		typeSpeed: 70,
 		loop: true,
 		startDelay: 1000,
