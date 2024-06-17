9f8390c3ea899e6ae8299a0a5fa99c4a97935346
Rahul  Nagarajan <rahul.nagarajan@peopletech.com>
2024-06-17 15:09:37 -0700

    Changes

--- js/script.js
+++ js/script.js
@@ -33,6 +33,8 @@ $(document).ready(function() {
 	});
 
 	$('.owl-carousel').owlCarousel({
+		autoplay:true,
+		autoplayTimeout: 2500,
 	    loop:true,
 	    items: 4,
 	    responsive:{
@@ -157,19 +159,5 @@ $(document).ready(function() {
 			body.removeClass("fixedNav");
 		}
 	}
-
-	$(document).ready(function(){
-	    $("#owl-slideshow").owlCarousel({
-	        autoplay: true,            
-	        autoplayTimeout: 6000,     
-	        loop: true,               
-	        responsive: {              
-	            0: { items: 1 },
-	            600: { items: 2 },
-	            1000: { items: 3 }
-	        }
-	    });
-	});
-
 
 });
