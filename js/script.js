24faf736492f80530e94822feb25dee7e08df714
Rahul Nagarajan <63788480+Rahul2K20@users.noreply.github.com>
2024-06-17 14:01:15 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -158,10 +158,18 @@ $(document).ready(function() {
 		}
 	}
 	
-	$("#skills").owlCarousel({
-	
-	autoPlay : 5000,
-	stopOnHover : false
-	)}
+	$(document).ready(function() {
+
+  $("#owl-slideshow").owlCarousel({
+    slideSpeed: 300,
+    loop: true,
+    autoPlay: true,
+    autoPlaySpeed: 5000,
+    autoPlayTimeout: 5000,
+    autoPlayHoverPause: false
+
+  });
+
+});
 
 });
